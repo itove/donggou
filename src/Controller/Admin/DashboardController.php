d6b6e5539a97246a5a96f5580785a0342afdc63a
Al Zee <z@alz.ee>
2024-09-23 05:54:20 +0800

    check admin crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use App\Entity\Order;
+use App\Entity\Check;
 use App\Entity\Node;
 use App\Entity\Region;
 use App\Entity\Page;
@@ -128,6 +129,7 @@ class DashboardController extends AbstractDashboardController
 
         yield MenuItem::section('');
         yield MenuItem::linkToCrud('Order Management', 'fas fa-book-open', Order::class);
+        yield MenuItem::linkToCrud('Check Records', 'fas fa-book-open', Check::class);
         yield MenuItem::linkToCrud('Menu Management', 'fas fa-link', Menu::class);
         yield MenuItem::linkToCrud('Tag Management', 'fas fa-tags', Tag::class);
         yield MenuItem::linkToCrud('Category Management', 'fas fa-table-cells-large', Category::class);
