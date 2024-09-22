943cd3c45f56ae87f00f8051087d2b7d5b1c1dba
Al Zee <z@alz.ee>
2024-09-23 05:56:49 +0800

    nth

--- src/Controller/Admin/CheckCrudController.php
+++ src/Controller/Admin/CheckCrudController.php
@@ -7,12 +7,21 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 
 class CheckCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Check::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable('new')
+            ;
     }
 
     /*
