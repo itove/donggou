dd10e5394c88d5d43f70562eb9c983b19ed2be68
Al Zee <z@alz.ee>
2024-10-23 09:06:46 +0800

    nth

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -579,7 +579,6 @@ class ApiController extends AbstractController
         $order->setWxPrepayId($resp['prepay_id']);
         $em->persist($order);
         $em->flush();
-        dump($order->getId());
 
         return $this->payWithWx($order->getId());
     }
